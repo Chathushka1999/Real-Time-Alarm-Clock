82c4dd566b04dc07e2af05796491b2403bc1276f
Sajeepan <Sajeepan1299@gmail.com>
2021-05-18 01:37:35 +0530

    UPdated with AVR basr micro product code and simulation file

---
+++ Alarm_timing/AVR_code/GccApplication1/main.cpp
@@ -0,0 +1,101 @@
+#define SPEAKER_PORT PORTD
+#define SPEAKER_DDR DDRD
+#define SPEAKER_PIN 0
+ 
+#define F_CPU 1000000L // CPU clock speed 1 MHz
+#include <avr/io.h>
+#include <util/delay.h> 
+#include "IncFile1.h"
+#include <stdlib.h>
+
+
+int snooze=0,alarm_status=0;
+void playMelody();
+void playNote(float duration, float frequency);
+void ring_alarm(int song[]);
+
+void setup(){
+	DDRB = 0b00000000;    // set the push button pins as input mode
+	PORTB = 0b00111100;
+	DDRD = 0b00011111;    // set the led /speaker connecting pins as an output mode
+	PORTD =0b00000000;    // set all the pins on off mode
+}
+
+void my_delay_ms(double ms){
+	while (0 < ms){
+		_delay_ms(1);
+		--ms;
+	}
+}
+
+int main(void){
+	setup();
+    while(1) {
+		if(!(PINB & (1<<5))){
+			alarm_status=1;
+		    ring_alarm(melody1);}
+		else if(!(PINB & (1<<4))){
+			alarm_status=1;
+			ring_alarm(melody2);}
+    }
+}
+
+ 
+ 
+void playNote(double duration, float frequency){
+    long int i,cycles;
+    double half_period;                // assigning the necessary variables
+    float wavelength;
+ 
+    wavelength=(1/frequency)*1000;    // find the wave length of the pulse from frequency
+    cycles=duration/wavelength;       // The number of cycles.
+    half_period = wavelength/2;       // The time between each toggle.
+    
+    SPEAKER_DDR |= (1 << SPEAKER_PIN);
+ 
+    for (i=0;i<cycles;i++){
+        my_delay_ms(half_period);                              // Wait 1 half wavelength.
+        SPEAKER_PORT |= (1 << SPEAKER_PIN);                    // Output 5 V to port Pin 7.
+        my_delay_ms(half_period);                              // Wait 1 half wavelength.
+        SPEAKER_PORT &= ~(1 << SPEAKER_PIN);                   // 0 V at port pin 7.
+    }
+ 
+    return; 
+}
+
+void ring_alarm(int song[]){
+	int notes=song[0];
+	int tempo=song[1];
+	int wholenote = (60000 * 4) / tempo;                                 // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+	int divider = 0;
+	double noteDuration = 0;
+	/* iterate over the notes of the melody.*/
+	for (int thisNote = 2; thisNote < notes * 2; thisNote = thisNote + 2) {
+		divider = song[thisNote + 1];                                    // calculates the duration of each note
+		if (divider > 0) {
+		noteDuration = (wholenote) / divider;}                           // regular note, just proceed
+		else if (divider < 0) {
+			noteDuration = (wholenote) / abs(divider);                   // dotted notes are represented with negative durations!!
+		noteDuration *= 1.5;}                                            // increases the duration in half for dotted notes
+		if (!(PINB & (1<<3))){
+			alarm_status=0;snooze=0;
+		break;}
+		else if((!(PINB & (1<<2))) && snooze<3 ){
+			snooze=snooze+1;
+			my_delay_ms(5000);
+			ring_alarm(song);}
+		else if((!(PINB & (1<<2))) && snooze==3 ){
+			snooze=0;alarm_status=0;
+			break;}
+		playNote( noteDuration * 0.9, song[thisNote]);                   // we only play the note for 90% of the duration, leaving 10% as a pause
+		my_delay_ms(noteDuration);                                       // Wait for the specif duration before playing the next note.
+		playNote(noteDuration, 0);                                       // stop the waveform generation before the next note.
+	}
+	if (snooze>=0 && snooze<3 && alarm_status==1){
+		snooze=snooze+1;
+		my_delay_ms(5000);
+		ring_alarm(song);}
+	else if(snooze==3 && alarm_status==1){
+		alarm_status=0;snooze=0;
+		return;}
+}
--- Alarm_timing/Arduino_code/Arduino_code.ino
+++ Alarm_timing/Arduino_code/Arduino_code.ino
@@ -92,7 +92,7 @@
 // tone 2 parameters
 
 
-int melody1[100] = {
+int melody1[102] = {50,200,
   NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16,
   NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_DS4,16, NOTE_F4,16,
   NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16, NOTE_G4,8, NOTE_C4,8, NOTE_E4,16, NOTE_F4,16,
@@ -102,24 +102,20 @@ int melody1[100] = {
   NOTE_DS4,16, NOTE_D4,16, NOTE_C4,-1
 };
 
-int melody2[] = {  
+int melody2[170] = {84,140,
   NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //1
   NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
   NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
   NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
-
   NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //5
   NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
   NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
   NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
-
   NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //9
   NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
   NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
-  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2
   };
-
-
 
 // defining the buttons
 #define snooz  A3
@@ -130,88 +126,57 @@ int melody2[] = {
 //define the Audio output pin
 #define speaker 7
 
-int snooze=0,alarm_status=0,size_tone=0;
-char delays[]={},frequency[] ={};
-//void melody_1(int []);
+int snooze=0,alarm_status=0;
 void setup() {
   pinMode(tone1, INPUT_PULLUP);
   pinMode(tone2, INPUT_PULLUP);
   pinMode(stop_alarm, INPUT_PULLUP);
   pinMode(snooz, INPUT_PULLUP);
-  Serial.begin(9600);
   pinMode(speaker, OUTPUT);
-  pinMode(8, OUTPUT);
-
 }
 
 void loop() {
-  if(digitalRead(tone1)==0 ){alarm_status=1;
-  //digitalWrite(8,HIGH);
-  melody_1();}
-  else if(digitalRead(tone2)==0 & alarm_status ==0){alarm_status=1;melody_2();}
+    if(digitalRead(tone1)==0 ){
+          alarm_status=1;
+          ring_alarm(melody1);}
+    else if(digitalRead(tone2)==0 & alarm_status ==0){
+          alarm_status=1;
+          ring_alarm(melody2);}
 }
 
-void melody_1(){
-  int tempo = 200;
-  int notes = sizeof(melody1) / sizeof(melody1[0]) / 2;
- 
-  int wholenote = (60000 * 4) / tempo;           // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
-  int divider = 0, noteDuration = 0;
-  
-    // iterate over the notes of the melody.
-    // Remember, the array is twice the number of notes (notes + durations)
-    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-      // calculates the duration of each note     
-      divider = melody1[thisNote + 1];
-      if (divider > 0) {
-        // regular note, just proceed
-        noteDuration = (wholenote) / divider;
-      } else if (divider < 0) {
-        noteDuration = (wholenote) / abs(divider);                // dotted notes are represented with negative durations!!
-        noteDuration *= 1.5;                                     // increases the duration in half for dotted notes
-      }
-      if (digitalRead(stop_alarm)==0){alarm_status=0;break;}
-      else if(digitalRead(snooz)==0){delay(3000);
-      //melody_1();
-      }
-      tone(speaker, melody1[thisNote], noteDuration * 0.9);    // we only play the note for 90% of the duration, leaving 10% as a pause
-      delay(noteDuration);                                     // Wait for the specief duration before playing the next note.
-      noTone(speaker);                                         // stop the waveform generation before the next note.
+void ring_alarm(int song[]){
+    int notes=song[0];
+    int tempo=song[1];
+    int wholenote = (60000 * 4) / tempo;                                   // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
+    int divider = 0, noteDuration = 0;
+    /* iterate over the notes of the melody.*/
+    for (int thisNote = 2; thisNote < notes * 2; thisNote = thisNote + 2) {
+          Serial.print(song[thisNote+1]);
+          divider = song[thisNote + 1];                                    // calculates the duration of each note 
+          if (divider > 0) {
+                  noteDuration = (wholenote) / divider;}                   // regular note, just proceed           
+          else if (divider < 0) {
+                  noteDuration = (wholenote) / abs(divider);               // dotted notes are represented with negative durations!!
+                  noteDuration *= 1.5;}                                    // increases the duration in half for dotted notes 
+          if (digitalRead(stop_alarm)==0){
+                  alarm_status=0;snooze=0;
+                  break;}
+          else if(digitalRead(snooz)==0 && snooze<3 ){
+                  snooze=snooze+1;
+                  delay(5000);
+                  ring_alarm(song);}
+          else if(digitalRead(snooz)==0 && snooze==3 ){
+                  snooze=0;alarm_status=0;
+                  break;}
+          tone(speaker, song[thisNote], noteDuration * 0.9);                // we only play the note for 90% of the duration, leaving 10% as a pause
+          delay(noteDuration);                                              // Wait for the specief duration before playing the next note.
+          noTone(speaker);                                                  // stop the waveform generation before the next note.
     }
- if (alarm_status==1){
-  delay(5000);
-  melody_1();}
-}
-
-void melody_2(){
-  int tempo = 140;
-  int notes = sizeof(melody2) / sizeof(melody2[0]) / 2;
- 
-  int wholenote = (60000 * 4) / tempo;           // this calculates the duration of a whole note in ms (60s/tempo)*4 beats
-  int divider = 0, noteDuration = 0;
-  
-    // iterate over the notes of the melody.
-    // Remember, the array is twice the number of notes (notes + durations)
-    for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
-      // calculates the duration of each note     
-      divider = melody2[thisNote + 1];
-      if (divider > 0) {
-        // regular note, just proceed
-        noteDuration = (wholenote) / divider;
-      } else if (divider < 0) {
-        noteDuration = (wholenote) / abs(divider);          // dotted notes are represented with negative durations!!
-        noteDuration *= 1.5;                                    // increases the duration in half for dotted notes
-      }
-      if (digitalRead(stop_alarm)==0){alarm_status=0;break;}
-      else if(digitalRead(snooz)==0){delay(3000);
-      //melody_1();
-      }
-      tone(speaker, melody2[thisNote], noteDuration * 0.9);    // we only play the note for 90% of the duration, leaving 10% as a pause
-      delay(noteDuration);                                     // Wait for the specief duration before playing the next note.
-      noTone(speaker);                                          // stop the waveform generation before the next note.
-    }
- if (alarm_status==1){
-  delay(5000);
-  melody_2();}
-  
+   if (snooze>=0 && snooze<3 && alarm_status==1){
+          snooze=snooze+1;
+          delay(5000);
+          ring_alarm(song);}
+   else if(snooze==3 && alarm_status==1){
+          alarm_status=0;snooze=0;
+          return;}
 }
